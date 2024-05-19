33d587d4b7a6c97c06683d8ec56235b81930aba7
Digo-dev <dev.ctbn@gmail.com>
2024-05-19 11:08:24 -0300

    commit antes da uma possivel mudança no ruo do projeto.

--- database/Produtos.py
+++ database/Produtos.py
@@ -2,5 +2,6 @@ PRODUTOS = [
     {'id': 1, 'Produto': 'Arroz', 'Unidade': 'Pacote', 'Peso': 5, 'Qtd': 120, 'Lote':'AJJS455SD5', 'Validade': '15/10/2024'},
     {'id': 2, 'Produto': 'Feijão', 'Unidade': 'Pacote', 'Peso': 1, 'Qtd': 220, 'Lote':'ASDF23423D6', 'Validade': '23/11/2024'},
     {'id': 3, 'Produto': 'Macarrão', 'Unidade': 'Pacote', 'Peso': 0.5, 'Qtd': 350, 'Lote':'21S4DF5DF21', 'Validade': '05/12/2024'},
-    {'id': 4, 'Produto': 'Farinha', 'Unidade': 'Pacote', 'Peso': 0.5, 'Qtd': 200, 'Lote':'DS321FS32D1', 'Validade': '08/08/2025'}
+    {'id': 4, 'Produto': 'Farinha', 'Unidade': 'Pacote', 'Peso': 0.5, 'Qtd': 200, 'Lote':'DS321FS32D1', 'Validade': '08/08/2025'},
+    {'id': 5, 'Produto': 'Pão', 'Unidade': 'Unidade', 'Peso': 0.1, 'Qtd': 0, 'Lote':'DS321FS32D1', 'Validade': '08/08/2025'}
 ]
--- routes/produto.py
+++ routes/produto.py
@@ -1,53 +1,58 @@
 from flask import Blueprint, render_template, request
 from database.Produtos import PRODUTOS
 
-
 produto_route = Blueprint('produto', __name__)
 
-"""
-Rota produtos
-
-    - /produto/ (GET)               -> Lista os produtos.
-    - /produto/ (POST)              -> Insere o produto no servidor.
-    - /produto/new (GET)            -> renderiza o formulario para criar um novo produto.
-    - /produto/<id> (GET)           -> Obter os dados de um produto.
-    - /produto/<id> (GET)           -> Renderiza um formulario para editar um produto.
-    - /produto/<id> (PUT)           -> atualizar os dados de um produto.
-    - /produto/<id>/delete (DELETE) -> Apaga o registro de um produto do servidor.
-"""
-
+"""Lista os produtos"""
 @produto_route.route('/')  
 def listar_produto():
-    """Lista os produtos"""
     return render_template('lista_produtos.html', produtos=PRODUTOS)
 
+"""Insere o produto no servidor."""
 @produto_route.route('/', methods=['POST'])
 def inserir_produto():
-    """Insere o produto no servidor."""
-    print(request.json)
-    return {"ok":"OK"}
+    
+    data = requests.json
 
-@produto_route.route('/new')
+    novo_produto = {
+        "id" : len(PRODUTOS)+1,
+        "Produto" : data['Produto'],
+        "Unidade" : data['Unidade'],
+        "Peso" : data['Peso'],
+        "Qtd" : data['Qtd'],
+        "Lote" : data['Lote'],
+        "Validade" : data['Validade']
+    }
+
+    PRODUTOS.append(novo_produto)
+
+    return render_template('item_produto.html', produto=novo_produto)
+    
+
+"""Renderiza o formulario para criar um novo produto."""
+@produto_route.route('/new') 
 def form_produto():
-    """Renderiza o formulario para criar um novo produto."""
     return render_template('form_produto.html')
 
+"""Obter os detalhes de um produto."""
 @produto_route.route('/<int:id_produto>')
 def detalhe_produto(id_produto):
-    """Obter os detalhes de um produto."""
     return render_template('detalhe_produto.html')
 
+"""Renderiza um formulario para editar um produto."""
 @produto_route.route('/<int:id_produto>/edit')
 def editar_produto(id_produto):
-    """Renderiza um formulario para editar um produto."""
     return render_template('form_edit_produto.html')
 
+"""atualizar os dados de um produto."""
 @produto_route.route('/<int:id_produto>/update',methods=['PUT'])
 def atualizar_produto(id_produto):
-    """atualizar os dados de um produto."""
     pass
 
-@produto_route.route('/<int:id_produto>/delete',methods=['DELETE'])
-def deletar_produto(id_produto):
-    """Apaga o registro de um produto do servidor."""
-    pass
+"""Apaga o registro de um produto do servidor."""
+@produto_route.route('/<int:produto_id>/delete',methods=['DELETE'])
+def deletar_produto(produto_id):
+    global PRODUTOS
+    PRODUTOS = [p for p in PRODUTOS if p['id'] != produto_id]
+
+    return {'deleted':'ok'}
